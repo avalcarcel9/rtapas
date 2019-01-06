0936cd5cb351d18e734be637c537093e4482ab19
avalcarcel9 <alval@pennmedicine.upenn.ed>
2019-01-06 12:30:47 -0500

    de-bugged gen_tapas_data.R previously gen_thresh_data.R. Created first draft of train_tapas.R. Cleans and builds with only one minor note locally

---
+++ R/gen_tapas_data.R
@@ -0,0 +1,94 @@
+#' @title Calculate Threshold Grid
+#' @description Calculates Sørensen's–Dice coefficient and automatic volume
+#'  estimation for a grid of thresholds.
+#' @param thresholds A \code{vector} of thresholds to use for calculation of Sørensen's–Dice coefficient
+#' and automatic volume. The default and grid applied in the published work is 0 to 1. Threshold
+#' values must be between 0 and 1.
+#' @param pmap A file path to a \code{NIFTI} probability map generated from an automatic segmentation
+#' approach or the name of a probability map locally available
+#' @param gold_standard A file path to the \code{NIFTI} gold standard segmentation approach corresponding
+#' with the probability map provided or the name of the gold standard segmentation that has been
+#' locally loaded. The gold standard segmentation is used to compare the thresholded probability map
+#' image with using Sørensen's–Dice coefficient.
+#' @param mask A file path to a \code{NIFTI} brain mask corresponding with the probability map provided
+#' or the name of a mask that has been locally loaded.
+#' @param k Minimum number of voxels for a cluster/component. Passed to \code{extransr::label_mask()}.
+#' @param subject_id is NULL by default. Must be a \code{character} vector with the unique subject ID for
+#' which the threshold data is being generated.
+#' @param verbose is TRUE by default. TRUE returns messages throughout the generating data function.
+#' FALSE will silence comment returns.
+#' @export
+#' @importFrom aliviateR dsc
+#' @importFrom dplyr bind_rows
+#' @importFrom extrantsr label_mask
+#' @importFrom magrittr "%>%"
+#' @importFrom neurobase check_nifti
+#' @importFrom tibble tibble
+#' @return A tibble with threshold, dsc, and volume columns
+#' @examples \dontrun{
+#' gen_tapas_data(thresholds = seq(from = 0, to = 1, by = 0.01),
+#' pmap = 'probability_map_subject_1.nii.gz',
+#' gold_standard = 'manual_segmentation_subject_1.nii.gz',
+#' mask = 'brain_mask_subject_1.nii.gz',
+#' k = 8,
+#' subject_id = 'subject_1')
+#' }
+
+gen_tapas_data <- function(thresholds = seq(from = 0, to = 1, by = 0.01), pmap, gold_standard, mask, k = 8, subject_id = NULL, verbose = TRUE){
+
+  # Check that verbose is TRUE or FALSE
+  if(is.logical(verbose) == FALSE){
+    stop('# verbose must be logical TRUE to return comments throughout the function or FALSE to silence comments.')
+  }
+
+  if(verbose == TRUE){
+    message('# Validating parameter inputs.')
+  }
+  # Verify inputs are NIFTI objects
+  pmap = neurobase::check_nifti(pmap)
+  gold_standard = neurobase::check_nifti(gold_standard)
+  mask = neurobase::check_nifti(mask)
+
+  # Check that grid is a vector from 0 to 1
+  if(is.numeric(thresholds) == FALSE | any(thresholds < 0) == TRUE | any(thresholds > 1) == TRUE){
+    stop('# thresholds must be a vector between 0 and 1.')
+  }
+
+  # Make pmap a vector to sapply over
+  pmap = c(pmap[mask == 1])
+
+  # Obtain a matrix of 0/1 after threhsolding at each threshold value
+  pred_lesion = sapply(thresholds, function(x) {ifelse(pmap > x, 1, 0)})
+
+  # initialize a results tibble
+  results = tibble::tibble(threshold = thresholds, dsc = thresholds, volume =  thresholds, subject_id = subject_id)
+
+  if(verbose == TRUE){
+    message('# Obtaining threshold level information.')
+  }
+
+  # function to calculate the volume and DSC at each threshold
+  calc_dv <- function(j, temp_lmask = mask, subject_id = subject_id){
+    # Fill in the predicted lesion values from threshold j to a temporary mask
+    temp_lmask[mask == 1] = pred_lesion[,j]
+
+    if(sum(temp_lmask) != 0){
+      # Label the lesion connected components
+      # Remove any lesion smaller than k connected components
+      temp_lmask = extrantsr::label_mask(temp_lmask, k = k)
+    }
+    # Return temp_lmask to binary 0/1
+    temp_lmask[temp_lmask > 0] = 1
+
+    results = tibble::tibble(threshold = thresholds[j],
+                             dsc = aliviateR::dsc(gold_standard = gold_standard, comp_method = temp_lmask),
+                             volume =  sum(temp_lmask),
+                             subject_id = subject_id)
+
+  }
+
+  results = lapply(1:length(thresholds), calc_dv, temp_lmask = mask, subject_id = subject_id) %>%
+    dplyr::bind_rows()
+
+  return(results)
+}
---
+++ R/train_tapas.R
@@ -0,0 +1,122 @@
+#' @title TAPAS Model Training
+#' @description  This function trains the TAPAS model as a \code{tibble} or \code{data.frame}
+#' produced by an element from the output of the function \code{\link{gen_tapas_data}}.
+#' @param data Data resulting from \code{\link{gen_tapas_data}}. May be in a binded
+#' \code{tibble} or \code{data.frame} form from all subjects to include for
+#' training or a \code{list} with all subject data in unique elements.
+#' @param dsc_cutoff is 0.03 by default. Must be a single value between 0 and 1. This is the DSC value that will be used as a cutoff.
+#' Only subjects with DSC greater than or equal to dsc_cutoff will be included in training the TAPAS model..
+#' @param verbose is TRUE by default. TRUE returns messages throughout the generating data function.
+#' FALSE will silence comment returns.
+#' @export
+#' @importFrom dplyr bind_rows filter group_by inner_join ungroup mutate select slice summarize
+#' @importFrom gtools inv.logit logit
+#' @importFrom magrittr "%>%"
+#' @importFrom mgcv gam predict.gam
+#' @importFrom stats quantile
+#' @importFrom tibble tibble is_tibble
+#' @return A \code{list} with the TAPAS model (tapas_model) in the first element of the list of class \code{gam} and the
+#' second element is a \code{tibble} with the clamp volume and threshold information (clamp_data).
+#' @examples \dontrun{
+#' # Put gen_tapas_data function here first then run train_tapas. Fix later.
+#' train_tapas(data = data, dsc_cutoff = 0.03, verbose = TRUE)
+#' }
+
+train_tapas <- function(data, dsc_cutoff = 0.03, verbose = TRUE){
+
+  # Check that verbose is TRUE or FALSE
+  if(is.logical(verbose) == FALSE){
+    stop('# verbose must be logical TRUE to return comments throughout the function or FALSE to silence comments.')
+  }
+
+  # Check that dsc_cutoff is between 0 and 1
+  if(dsc_cutoff < 0 | dsc_cutoff > 1){
+    stop('# dsc_cutoff must be a single value between 0 and 1.')
+  }
+
+  if(verbose == TRUE){
+    message('# Validating data input.')
+  }
+
+  # Create full subject tibble by binding the list rows or verify the data provided is a data.frame or tibble
+  if(is.list(data) == TRUE){
+    data = dplyr::bind_rows(data)
+  } else if (is.data.frame(data) == FALSE & tibble::is_tibble(data) == FALSE){
+    stop('# data must be a list, data.frame, or tibble of stacked subject data objects from rtapas::gen_tapas_data(). \n
+          # The rtapas::gen_tapas_data() function returns a tibble by default.')
+  }
+
+  if(verbose == TRUE){
+    message('# Calculating group and subject-specific threshold and volume values.')
+  }
+
+  # Calculate subject level threshold that produces maximum DSC
+  subject_thresholds = data %>%
+    dplyr::group_by(subject_id) %>%
+    dplyr::slice(which.max(dsc)) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(-volume)
+
+  # Calculate the threshold that maximizes group level average DSC
+  group_threshold = data %>%
+    dplyr::group_by(threshold) %>%
+    dplyr::summarize(mean_dsc = mean(dsc)) %>%
+    dplyr::slice(which.max(mean_dsc)) %>%
+    dplyr::select(threshold)
+
+  # Obtain the group level volume from using the group_threshold
+  group_volumes = data %>%
+    dplyr::group_by(subject_id) %>%
+    dplyr::filter(threshold == group_threshold$threshold) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(-dsc, -threshold)
+
+  # Merge the group volume with the best thresholds
+  ## This contains the subject specific threshold that maximized DSC
+  ## The dsc value produced using the subject specific threshold
+  ## The volume produced using the group threshold
+  ## The unique subject_id
+  data = dplyr::inner_join(x = subject_thresholds, y = group_volumes, by = c("subject_id" = "subject_id"))
+
+  # Check for subjects with DSC less than the dsc_cutoff
+  if(any(data$dsc < dsc_cutoff)){
+
+    if(verbose == TRUE){
+      message('# Poor DSC detected excluding subject(s) from training the TAPAS model.')
+    }
+
+    # Remove subjects from training with poor DSC
+    data = data %>%
+      dplyr::filter(dsc >= dsc_cutoff)
+  }
+
+  # logit transformation cannot handle 0 or 1 exact values so add a check to make these values
+  # e^(-16) or  0.999999999 so logit will not error
+  if(any(data$threshold == 0) | any(data$threshold == 1)){
+    data = data %>%
+      dplyr::mutate(threshold = replace(threshold, threshold == 0, exp(-16)),
+                    threshold = replace(threshold, threshold == 1, 0.999999999))
+  }
+
+  if(verbose == TRUE){
+    message('# Fitting the TAPAS model.')
+  }
+
+  # Fit the TAPAS model
+  tapas_model = mgcv::gam(formula = gtools::logit(threshold) ~ s(volume),
+                          data = data)
+
+  if(verbose == TRUE){
+    message('# Calculating lower and upper bound clamps.')
+  }
+
+  # Based on the training data obtain the volume associated with the 10th and 90th percentile
+  # Use these volumes to predict a threshold
+  clamp_data = tibble::tibble(volume = c(stats::quantile(data$volume, .1), stats::quantile(data$volume, .9))) %>%
+    dplyr::mutate(pred_threshold = gtools::inv.logit(mgcv::predict.gam(tapas_model, .)),
+                  bound = c('lower', 'upper')) %>%
+    dplyr::select(bound, volume, pred_threshold)
+
+  return(list(tapas_model = tapas_model, clamp_data = clamp_data))
+
+}
