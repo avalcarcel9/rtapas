802a64e4ab69cc6da1e72af72cf54ed9eb31e4ed
avalcarcel9 <alval@pennmedicine.upenn.ed>
2019-02-17 17:41:19 -0500

    adding brain mask and gs fake data

---
+++ R/data.R
@@ -0,0 +1,129 @@
+#' @title Brain Mask
+#' @description Brain mask for all probability maps and gold standard masks.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name brain_mask
+#' @docType data
+#' @usage brain_mask
+NULL
+
+#' @title Gold Standard 1
+#' @description Gold standard segmentation for subject 1.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs1
+#' @docType data
+#' @usage gs1
+NULL
+
+#' @title Gold Standard 2
+#' @description Gold standard segmentation for subject 2.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs2
+#' @docType data
+#' @usage gs2
+NULL
+
+#' @title Gold Standard 3
+#' @description Gold standard segmentation for subject 3.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs3
+#' @docType data
+#' @usage gs3
+NULL
+
+#' @title Gold Standard 4
+#' @description Gold standard segmentation for subject 4.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs4
+#' @docType data
+#' @usage gs4
+NULL
+
+#' @title Gold Standard 5
+#' @description Gold standard segmentation for subject 5.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs5
+#' @docType data
+#' @usage gs5
+NULL
+
+#' @title Gold Standard 6
+#' @description Gold standard segmentation for subject 6.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs6
+#' @docType data
+#' @usage gs6
+NULL
+
+#' @title Gold Standard 7
+#' @description Gold standard segmentation for subject 7.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs7
+#' @docType data
+#' @usage gs7
+NULL
+
+#' @title Gold Standard 8
+#' @description Gold standard segmentation for subject 8.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs8
+#' @docType data
+#' @usage gs8
+NULL
+
+#' @title Gold Standard 9
+#' @description Gold standard segmentation for subject 9.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs9
+#' @docType data
+#' @usage gs9
+NULL
+
+#' @title Gold Standard 10
+#' @description Gold standard segmentation for subject 10.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs10
+#' @docType data
+#' @usage gs10
+NULL
+
+#' @title Gold Standard 11
+#' @description Gold standard segmentation for subject 11.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs11
+#' @docType data
+#' @usage gs11
+NULL
+
+#' @title Gold Standard 12
+#' @description Gold standard segmentation for subject 12.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs12
+#' @docType data
+#' @usage gs12
+NULL
+
+#' @title Gold Standard 13
+#' @description Gold standard segmentation for subject 13.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs13
+#' @docType data
+#' @usage gs13
+NULL
+
+#' @title Gold Standard 14
+#' @description Gold standard segmentation for subject 14.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs14
+#' @docType data
+#' @usage gs14
+NULL
+
+#' @title Gold Standard 15
+#' @description Gold standard segmentation for subject 15.
+#' This data was created by Alessandra Valcarcel and is artificial for example purposes.
+#' @name gs15
+#' @docType data
+#' @usage gs15
+NULL
+
+
---
+++ R/mask.R
@@ -0,0 +1,6 @@
+#' @title <Brief Description>
+#' @description <Full Description>
+#' @name object_name
+#' @docType data
+#' @usage object_name
+#' @keywords <Insert key words if you'd like>
